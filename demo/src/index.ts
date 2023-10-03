7d31bb4b48d39477bb8b21c14d74ac2c61e6b944
jill64 <intents.turrets0h@icloud.com>
2023-10-03 20:08:18 +0900

    fix: demo api

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -3,6 +3,7 @@ import type { OriginRequestHandler } from '@jill64/types-lambda'
 export const handler: OriginRequestHandler<'custom'> = () => {
   return {
     status: '200',
-    body: 'Hello, world!'
+    body: 'Hello, world!',
+    bodyEncoding: 'text'
   }
 }
