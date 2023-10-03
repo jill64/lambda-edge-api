8164b2974cd2f2bea64f1112f430c3f6a95c63cb
jill64 <intents.turrets0h@icloud.com>
2023-10-03 21:04:30 +0900

    fix: test value

--- demo/test/index.test.ts
+++ demo/test/index.test.ts
@@ -5,5 +5,5 @@ test('test', async () => {
   if (!url) throw new Error('PREVIEW_URL is not defined')
   const response = await fetch(url)
   const text = await response.text()
-  expect(text).toBe('Hello World!')
+  expect(text).toBe('Hello, World!')
 })
